0fd35d72d1f1ecc91810bf8e9fbc2b9516ab8697
Lorenzo Rizzotti <10401566+Dreaming-Codes@users.noreply.github.com>
2021-04-09 11:28:05 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,5 +6,10 @@ description 'A boilerplate for using Typescript in FiveM'
 author 'Remco Troost (d0p3t)'
 url 'https://github.com/d0p3t/fivem-ts-boilerplate'
 
+dependency 'yarn'
+dependency 'webpack'
+
+webpack_config 'webpack.config.js'
+
 client_script 'dist/client/*.client.js'
 server_script 'dist/server/*.server.js'
